201139f96dcef71c30c13afb841d6cf9ed2a4184
9ouo9pp <9ouo9pp@gmail.com>
2025-07-10 13:22:04 +0900

    컨텐츠 html 작성 완료, 모달 작성중

--- js/main.js
+++ js/main.js
@@ -1,51 +1,42 @@
 gsap.registerPlugin(ScrollTrigger);
 
-function aniMainTitle() {
-  const span1 = document.querySelectorAll(".section1 .txt1 span");
-  const span2 = document.querySelectorAll(".section1 .txt2 span");
+// function aniMainTitle() {
+//   const span1 = document.querySelectorAll(".section1 .txt1 span");
+//   const span2 = document.querySelectorAll(".section1 .txt2 span");
 
-  // random 순서 배열
-  // function shuffle(arr) {
-  //     return arr.from(arr).sort(() => Math.random() - 0.25)
-  // }
-  function shuffle(arr) {
-    return [...arr].sort(() => Math.random() - 0.5);
-  }
+//   // random 순서 배열
+//   // function shuffle(arr) {
+//   //     return arr.from(arr).sort(() => Math.random() - 0.25)
+//   // }
+//   function shuffle(arr) {
+//     return [...arr].sort(() => Math.random() - 0.5);
+//   }
 
-  // 랜덤한 배열에 맞춰 show클래스 추가
-  const randomOrder1 = shuffle([...span1.keys()]);
-  const randomOrder2 = shuffle([...span2.keys()]);
-  const randomOrder3 = shuffle([...span3.keys()]);
+//   // 랜덤한 배열에 맞춰 show클래스 추가
+//   const randomOrder1 = shuffle([...span1.keys()]);
+//   const randomOrder2 = shuffle([...span2.keys()]);
+//   const randomOrder3 = shuffle([...span3.keys()]);
 
-  randomOrder1.forEach((index, i) => {
-    setTimeout(() => {
-      span1[index].classList.add("show");
-    }, i * 200);
-  });
-  randomOrder2.forEach((index, i) => {
-    setTimeout(() => {
-      span2[index].classList.add("show");
-    }, i * 200);
-  });
-  randomOrder3.forEach((index, i) => {
-    setTimeout(() => {
-      span3[index].classList.add("show");
-    }, i * 200);
-  });
-}
+//   randomOrder1.forEach((index, i) => {
+//     setTimeout(() => {
+//       span1[index].classList.add("show");
+//     }, i * 200);
+//   });
+//   randomOrder2.forEach((index, i) => {
+//     setTimeout(() => {
+//       span2[index].classList.add("show");
+//     }, i * 200);
+//   });
+//   randomOrder3.forEach((index, i) => {
+//     setTimeout(() => {
+//       span3[index].classList.add("show");
+//     }, i * 200);
+//   });
+// }
 
 // aniMainTitle();
 
-// gsap.set(".visual .bg", {
-//   clipPath: "inset(0 " + per + "%)",
-//   scale: "0.9"
-// });
-// gsap.set(".visual .bg", {
-//   width: "455px",
-//   height: "688px",
-//   borderRadius: "260px"
-//   //  scale: 0.8,
-// });
+// intro
 gsap.set(".visual .bg", {
   width: "20vw",
   height: "64vh",
@@ -74,15 +65,6 @@ const tl = gsap.timeline({
   }
 });
 
-// tl.to(".visual .bg", {
-//   clipPath: "inset(0% 0%)",
-//   //  scale: 1,
-//   duration: 2,
-//   width: "100vw",
-//   height: "100vh",
-//   borderRadius: "0px",
-//   ease: "power2.inOut"
-// });
 tl.to(".visual .bg", {
   clipPath: "inset(0% 0%)",
   duration: 2,
@@ -93,3 +75,5 @@ tl.to(".visual .bg", {
 });
 
 tl.to({}, { duration: 2 });
+
+// pin page
